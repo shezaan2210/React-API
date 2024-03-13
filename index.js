516bce4c2ec31667ac6703c604d15e28c9cc77b7
Mohammad Shezaan Ansari <shezaanansari2210@gmail.com>
2024-03-13 17:28:18 +0530

    Day-2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
 require('dotenv').config({path: './.env'});
+const express = require('express');
 const app = express();
-console.log('hello')
+require('./models/config').dbconnection();
+
+
+
+console.log('hello');
 app.listen(process.env.PORT, ()=>{
     console.log(`server is running on port ${process.env.PORT}`);
 });
---
+++ models/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config({path: './.env'});
+const mongoose = require('mongoose');
+
+exports.dbconnection = async ()=>{
+    try{
+        mongoose.connect(process.env.MONGO_URL);
+        console.log('Connection Established');
+    }
+    catch (error){
+        console.log(error.message)
+    }
+}
